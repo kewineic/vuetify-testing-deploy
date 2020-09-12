75bd98f3e228648100355b475f9542c49aaabf75
kewineic <kewin.ferreira@hotmail.com>
2020-09-12 18:27:01 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
